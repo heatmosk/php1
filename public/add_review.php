705cdf284a506afa77fec9ed6d19331548ca1821
heatmosk <heat@mosk.ru>
2020-05-15 23:06:05 +0300

    lesson 6

---
+++ public/add_review.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../config/main.php';
+require_once ENGINE_DIR . 'render.php';
+require_once ENGINE_DIR . 'base.php';
+require_once ENGINE_DIR . 'db.php';
+$mainMenu = include_once CONFIG_DIR . 'menu.php';
+
+$mainMenuHTML = renderMenu($mainMenu);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST["id"]) && isset($_POST["rating"]) && isset($_POST["review_text"])) {
+    dbAddProductReview($_POST["id"], $_POST["rating"], htmlentities(strip_tags($_POST["review_text"])));
+  }
+  redirect("/view_product.php?id=" . $_POST["id"]);
+}
+
+if (!isset($_GET["id"])) {
+  redirect("/");
+}
+
+$productId = (int) $_GET["id"];
+
+
+include VIEWS_DIR . "header.php";
+?>
+
+<div><?= $mainMenuHTML ?></div>
+
+<?php
+
+include VIEWS_DIR . 'review_form.php';
+include VIEWS_DIR . "footer.php";
+
+?>
---
+++ public/view_product.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../config/main.php';
+require_once ENGINE_DIR . 'render.php';
+require_once ENGINE_DIR . 'fs.php';
+require_once ENGINE_DIR . 'base.php';
+require_once ENGINE_DIR . 'db.php';
+$mainMenu = include_once CONFIG_DIR . 'menu.php';
+
+$mainMenuHTML = renderMenu($mainMenu);
+
+if (!isset($_GET["id"])) {
+  redirect("/catalog.php");
+}
+
+$productId = (int) $_GET["id"];
+$product = dbGetProduct($productId);
+$reviews = dbGetProductReviews($productId);
+$reviewsCounter = count($reviews);
+dbAddProductView($productId);
+$views = $product["views"] + 1;
+
+include VIEWS_DIR . "header.php";
+ 
+?>
+
+<div><?= $mainMenuHTML ?></div>
+
+<?php
+include VIEWS_DIR . "view_product.php";
+include VIEWS_DIR . "footer.php";
+?>
