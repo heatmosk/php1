c545193c30a0a98db1e07d143fc155d086d32ac9
heatmosk <heat@mosk.ru>
2020-05-18 23:43:49 +0300

    m

--- views/login_form.php
+++ views/login_form.php
@@ -1,4 +1,4 @@
-<form method="POST">
+<form action="" enctype="multipart/form-data" method="POST">
   <div><label>Логин</label>:<input type="text" name="login" required value="" /></div>
   <div><label>Пароль</label>:<input type="password" name="password" required value="" /></div>
   <div><input type="submit" value="Войти" /></div>
