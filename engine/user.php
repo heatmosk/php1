fea3a2e0b40a4da6f65b26f47ee065693e5df1cb
heatmosk <heat@mosk.ru>
2020-05-18 23:56:32 +0300

    m

--- engine/user.php
+++ engine/user.php
@@ -6,15 +6,11 @@ require_once "base.php";
 function getUserByLogin($login)
 {
   $userLogin = mysqli_escape_string(dbConnect(), $login);
-  $query = "SELECT * FROM `users` WHERE `login` = {$userLogin}";
-  echo $query . "<br>";
+  $query = "SELECT * FROM `users` WHERE `login` = '{$userLogin}'"; 
   return dbQueryOne($query);
 }
 
 function checkUserPassword($login, $password): bool
 {
-  echo "login: " . $login . "<br>";
-  echo "password: " . $password . "<br>";
-  echo "hash: " . getHash($password) . "<br>";
   return getHash($password) == getUserByLogin($login)["pass"];
 }
