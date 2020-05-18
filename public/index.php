5c8b95c94c9283d257ae688215629796ce5063c9
heatmosk <heat@mosk.ru>
2020-05-19 00:59:51 +0300

    m

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require_once ENGINE_DIR . "user.php";
 require_once ENGINE_DIR . "session.php";
 require_once ENGINE_DIR . 'render.php';
 
-$page = isset($_GET) ? get($page) : "index";
+$page = isset($_GET["page"]) ? get("page") : "index";
 
 $params = [];
 $params["menu"] = renderMenu(include_once CONFIG_DIR . 'menu.php');
@@ -14,7 +14,7 @@ if (session("user_id")) {
   $params["user"] = getUserById(session("user_id"));
 }
 
-echo render($page, $params);
+echo render("main", ["content" => render($page, $params)]);
 
 // require_once __DIR__ . '/../config/main.php';
 // require_once ENGINE_DIR . "base.php";
