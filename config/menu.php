ffa062445758d09206443b82065d8897a3152b72
heatmosk <heat@mosk.ru>
2020-05-19 01:10:23 +0300

    m

--- config/menu.php
+++ config/menu.php
@@ -2,9 +2,9 @@
 
 return [
   'Home'    => ['url' => '/'],
-  'Gallery' => ['url' => '/gallery.php'],
-  'Catalog' => ['url' => '/catalog.php'],
-  'Login'   => ['url' => '/login.php'],
+  'Gallery' => ['url' => '/?page=gallery'],
+  // 'Catalog' => ['url' => '/catalog.php'],
+  // 'Login'   => ['url' => '/login.php'],
   // 'Calc 1' => ['url' => '/calc1.php'],
   // 'Calc 2' => ['url' => '/calc2.php'],
 ];
