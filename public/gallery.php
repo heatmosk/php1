03693868c817e320f18a51dafe317158bf1c3c31
heatmosk <heat@mosk.ru>
2020-05-19 01:23:46 +0300

    m

--- public/gallery.php
+++ public/gallery.php
@@ -1,12 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../config/main.php';
-require_once VENDOR_DIR . "funcImgResize.php";
-require_once ENGINE_DIR . 'render.php';
-require_once ENGINE_DIR . 'fs.php';
-require_once ENGINE_DIR . 'base.php';
-require_once ENGINE_DIR . 'db.php'; 
-
 $acceptedImages =  ['jpg', 'jpeg', 'png'];
 $acceptedImagesFilter = "." . implode(", .", $acceptedImages);
 
