e2173ff3cc9f65f57bc7ce604f3795495fb16136
heatmosk <heat@mosk.ru>
2020-05-19 13:42:03 +0300

    m

--- public/view_product.php
+++ public/view_product.php
@@ -7,6 +7,7 @@ if (!isset($_GET["id"])) {
 $productId = (int) get("id");
 $product = getProductById($productId);
 $reviews = getProductReviews($productId);
+$product["image"] = getProductImage($product["image_id"]);
 $reviewsCounter = count($reviews);
 addProductView($productId);
 $views = $product["views"] + 1;
--- views/view_product.php
+++ views/view_product.php
@@ -1,10 +1,10 @@
 <div class='content'>
   <div class='images'>
-    <img src='<?= $product['filename'] ?>'>
+    <img src='<?= $product["image"]['filename'] ?>'>
   </div>
   <div class='reviews'>
     <div class='reviews__header'>Отзывы: <?= $reviewsCounter == 0 ? "отсутствуют" : $reviewsCounter ?></div>
-    <a href="/add_review.php?id=<?= $productId ?>">Добавить отзыв</a>
+    <a href="/?page=add_review&id=<?= $productId ?>">Добавить отзыв</a>
     <h3>Наименование товара: <?= $product['product_name'] ?></h3>
     <div>Описание: <?= $product['product_description'] ?></div>
     <h4>Количество просмотров: <?= $views ?></h4>
