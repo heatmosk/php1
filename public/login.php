f330fe57a389580eb4c44be19673cc38249405ac
heatmosk <heat@mosk.ru>
2020-05-19 09:00:45 +0300

    m

--- public/login.php
+++ public/login.php
@@ -4,14 +4,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $login = post("login");
   $pass  = post("password");
   echo $login . ' ' . $pass . "<br>";
-  // if (checkUserPassword($login, $pass)) {
-  //   $user = getUserByLogin($login);
-  //   setSession("user_id", $user["id"]);
-  //   $message = "ВСЁ ОК"; 
-  //   // redirect("/");
-  // } else {
-  //   $message = "Неверное имя пользователя или пароль";
-  // }
+  if (checkUserPassword($login, $pass)) {
+    echo "success<br>";
+    // $user = getUserByLogin($login);
+    // setSession("user_id", $user["id"]);
+    // $message = "ВСЁ ОК"; 
+    // redirect("/");
+  } else {
+    $message = "Неверное имя пользователя или пароль";
+  }
   // return compact("message");
   return [];
 }
