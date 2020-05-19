4eb97dff67b26143f533f0cef830cbc1e305b056
heatmosk <heat@mosk.ru>
2020-05-19 08:19:12 +0300

    m

--- public/index.php
+++ public/index.php
@@ -3,13 +3,14 @@
 require_once __DIR__ . '/../config/main.php';
 require_once ENGINE_DIR . "base.php";
 require_once ENGINE_DIR . "fs.php";
-require_once ENGINE_DIR . "db.php"; 
+require_once ENGINE_DIR . "db.php";
 require_once ENGINE_DIR . "session.php";
 require_once ENGINE_DIR . "user.php";
 require_once ENGINE_DIR . "gallery.php";
 require_once ENGINE_DIR . 'render.php';
 
-$page = isset($_REQUEST["page"]) ? request("page") : "index"; 
+$page = isset($_REQUEST["page"]) ? request("page") : "index";
+echo "page: {$page}<br>";
 
 $params = [];
 if (sessionGet("user_id")) {
@@ -17,9 +18,11 @@ if (sessionGet("user_id")) {
 }
 $params['title'] = "PAGE: " . $page;
 
-if ($page !== "index") { 
+if ($page !== "index") {
+  echo PUBLIC_DIR . $page . ".php<br>";
   $params = array_merge($params, include PUBLIC_DIR . $page . ".php");
 }
+
 echo render(VIEWS_DIR . "main", [
   "menu" => renderMenu(include_once CONFIG_DIR . 'menu.php'),
   "content" => render(VIEWS_DIR . $page, $params)
