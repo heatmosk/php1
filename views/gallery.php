a10995239b4a8e08cba14bedad2fc9ca2bd432a4
heatmosk <heat@mosk.ru>
2020-05-19 08:36:16 +0300

    m

--- views/gallery.php
+++ views/gallery.php
@@ -1,7 +1,7 @@
 
 <div>
   <?php foreach ($galleryImages as $image) : ?>
-    <a href='/view_image.php?id=<?= $image['id'] ?>'>
+    <a href='/?page=view_image&id=<?= $image['id'] ?>'>
       <div class='gallery__item'><img src='<?= $image['previewfile'] ?>'></div>
     </a>
   <?php endforeach; ?>
