597f168a29ee3198bca00cb798eeea4d13bf9131
heatmosk <heat@mosk.ru>
2020-05-19 08:58:58 +0300

    m

--- public/login.php
+++ public/login.php
@@ -3,15 +3,17 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $login = post("login");
   $pass  = post("password");
-  if (checkUserPassword($login, $pass)) {
-    $user = getUserByLogin($login);
-    setSession("user_id", $user["id"]);
-    $message = "ВСЁ ОК"; 
-    // redirect("/");
-  } else {
-    $message = "Неверное имя пользователя или пароль";
-  }
-  return compact("message");
+  echo $login . ' ' . $pass . "<br>";
+  // if (checkUserPassword($login, $pass)) {
+  //   $user = getUserByLogin($login);
+  //   setSession("user_id", $user["id"]);
+  //   $message = "ВСЁ ОК"; 
+  //   // redirect("/");
+  // } else {
+  //   $message = "Неверное имя пользователя или пароль";
+  // }
+  // return compact("message");
+  return [];
 }
 
 return [];
--- views/login.php
+++ views/login.php
@@ -1,4 +1,4 @@
-<form action="" enctype="multipart/form-data" method="POST">
+<form action="/?page=login" enctype="multipart/form-data" method="post">
   <div><label>Логин</label>:<input type="text" name="login" required value="" /></div>
   <div><label>Пароль</label>:<input type="password" name="password" required value="" /></div>
   <div><input type="submit" value="Войти" /></div>
